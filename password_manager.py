876d7bfa9c50687169bec1061646d5e9bca1b58d
Adeyemo-Miracle <adeyemomoyin@gmail.com>
2024-01-30 21:57:58 +0100

    final commit

--- password_manager.py
+++ password_manager.py
@@ -15,23 +15,3 @@ while True:
     else:
         print('Invalid mode. ')
         continue
-    
-pwd = input('What is the master password?')
-def view():
-    pass
-def add():
-    pass
-
-while True:
-    mode = input('would you like add a new password or view existing ones (view/add), press "q" to quit ')
-    if mode == 'q':
-        break
-    if mode == 'view':
-        pass
-    elif mode == 'add':
-        pass
-    else:
-        print('Invalid mode. ')
-        continue
-    
-    
