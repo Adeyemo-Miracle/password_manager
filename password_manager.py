9952bd70af42ae6b3bd346541ddce2811e02e06d
Adeyemo-Miracle <114000181+Adeyemo-Miracle@users.noreply.github.com>
2024-02-27 11:13:37 +0100

    final commit

--- password_manager.py
+++ password_manager.py
@@ -58,21 +58,6 @@ def view():
             user, passw = data.split('|')
 def add():
     name= input('Account Name: ')
-    pwd = input('Password: ')
     
-    with open('passwords.txt', 'a') as f:
-        f.write(name + '|' + pwd + '\n')
-
-while True:
-    mode = input('would you like add a new password or view existing ones (view/add), press "q" to quit ')
-    if mode == 'q':
-        break
-    if mode == 'view':
-        view()
-    elif mode == 'add':
-        add()
-    else:
-        print('Invalid mode. ')
-        continue
 
 
