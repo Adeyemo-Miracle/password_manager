699b59d6ee34b1a98fb9a6a2165ea982d1ebd776
Adeyemo-Miracle <114000181+Adeyemo-Miracle@users.noreply.github.com>
2024-03-03 10:25:00 +0100

    Final commit

--- password_manager.py
+++ password_manager.py
@@ -1,5 +1,3 @@
-
-
 pwd = input('What is the master password?')
 def view():
     with open('passwords.txt', 'r') as f:
@@ -12,6 +10,25 @@ def add():
     
     with open('passwords.txt', 'a') as f:
         f.write(name + '|' + pwd + '\n')
+
+while True:
+    mode = input('would you like add a new password or view existing ones (view/add), press "q" to quit ')
+    if mode == 'q':
+        break
+    if mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print('Invalid mode. ')
+        continue
+
+
+
+
+
+
+
 
 while True:
     mode = input('would you like add a new password or view existing ones (view/add), press "q" to quit ')
